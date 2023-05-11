2757b6ef22d3ca095b44abe47c2ca5b3e678b380
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2023-05-11 11:47:29 -0500

    Added project for CLGen

---
+++ src/CLGen/main.c
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // TODO: Implement WinMain
+
+    MessageBoxA(NULL, "CLGen not implemented yet.", "Error", MB_OK | MB_ICONERROR);
+
+    return 0;
+}
