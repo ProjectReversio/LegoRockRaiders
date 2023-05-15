971f694b81efd4025194f8fb12b458298f3546d6
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2023-05-15 17:46:52 -0500

    Stub defines for dx debugging files

---
+++ src/LegoRR/dxbug.c
@@ -0,0 +1,2 @@
+#include "dxbug.h"
+
---
+++ src/LegoRR/dxbug.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "types.h"
+
+// TODO: Implement these
+#define CHKRM(hres)
+#define CHKDD(hres)
+#define CHKDI(hres)
+#define CHKIU(hres)
