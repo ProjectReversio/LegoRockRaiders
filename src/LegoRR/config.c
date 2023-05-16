0c31d4123db1be6b8ed798d02e1e456d1cfee378
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2023-05-16 17:38:46 -0500

    Implemented Config_Initialize, Config_Shutdown, and Config_BuildStringID

--- src/LegoRR/config.c
+++ src/LegoRR/config.c
@@ -1,11 +1,49 @@
+#include <string.h>
+#include <stdarg.h>
 #include "config.h"
+#include "mem.h"
+
+Config_Globs configGlobs = { NULL };
 
 void Config_Initialize()
 {
-    // TODO: Implement Config_Initialize
+    for (U32 loop = 0; loop < CONFIG_MAXLISTS; loop++)
+    {
+        configGlobs.listSet[loop] = NULL;
+    }
+
+    configGlobs.freeList = NULL;
+    configGlobs.listCount = 0;
+    configGlobs.flags = CONFIG_GLOB_FLAG_INITIALIZED;
 }
 
 void Config_Shutdown()
 {
-    // TODO: Implement Config_Shutdown
+    for (U32 loop = 0; loop < CONFIG_MAXLISTS; loop++)
+    {
+        if (configGlobs.listSet[loop])
+            Mem_Free(configGlobs.listSet[loop]);
+    }
+
+    configGlobs.freeList = NULL;
+    configGlobs.flags = CONFIG_GLOB_FLAG_NONE;
+}
+
+const char* Config_BuildStringID(const char* s, ...)
+{
+    va_list args;
+    static char string[1024];
+    char* curr;
+
+    strcpy(string, s);
+
+    va_start(args, s);
+    while ((curr = va_arg(args, char*)))
+    {
+        strcat(string, CONFIG_SEPARATOR);
+        strcat(string, curr);
+    }
+    va_end(s);
+
+    return string;
 }
--- src/LegoRR/config.h
+++ src/LegoRR/config.h
@@ -2,6 +2,17 @@
 
 #include "types.h"
 
+#define CONFIG_MAXLISTS 32
+
+#define CONFIG_SEPARATOR "::"
+
+typedef enum Config_GlobFlags
+{
+    CONFIG_GLOB_FLAG_NONE = 0,
+    CONFIG_GLOB_FLAG_INITIALIZED = 1,
+    CONFIG_GLOB_FLAG_LOADINGCONFIG = 2,
+} Config_GlobFlags;
+
 typedef struct Config
 {
     char* fileData;
@@ -15,5 +26,17 @@ typedef struct Config
     struct Config* nextFree;
 } Config, *lpConfig;
 
+typedef struct Config_Globs
+{
+    lpConfig listSet[CONFIG_MAXLISTS];
+    lpConfig freeList;
+    U32 listCount;
+    Config_GlobFlags flags;
+} Config_Globs, *lpConfig_Globs;
+
+extern Config_Globs configGlobs;
+
 extern void Config_Initialize();
 extern void Config_Shutdown();
+
+extern const char* Config_BuildStringID(const char* s, ...);
