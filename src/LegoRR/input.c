4d38399541a68ca90b555835244013e7fdf8e708
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2023-06-28 19:11:29 -0500

    Implemented Input_ReadMouse2

--- src/LegoRR/input.c
+++ src/LegoRR/input.c
@@ -1,4 +1,5 @@
 #include "input.h"
+#include "main.h"
 #include <windows.h>
 
 Input_Globs inputGlobs = { NULL };
@@ -21,7 +22,32 @@ void Input_ReadKeys()
 
 void Input_ReadMouse2()
 {
-    // TODO: Implement Input_ReadMouse2
+    RECT rect;
+    POINT clientPos, cursorPos;
+
+    if (GetClientRect(Main_hWnd(), &rect))
+    {
+        clientPos.x = clientPos.y = 0;
+        ClientToScreen(Main_hWnd(), &clientPos);
+        GetCursorPos(&cursorPos);
+
+        cursorPos.x -= clientPos.x;
+        cursorPos.y -= clientPos.y;
+
+        if (cursorPos.x >= appWidth())
+            cursorPos.x = appWidth() - 1;
+        if (cursorPos.y >= appHeight())
+            cursorPos.y = appHeight() - 1;
+        if (cursorPos.x < 0)
+            cursorPos.x = 0;
+        if (cursorPos.y < 0)
+            cursorPos.y = 0;
+
+        inputGlobs.diffx = cursorPos.x - inputGlobs.msx;
+        inputGlobs.diffy = cursorPos.y - inputGlobs.msy;
+        inputGlobs.msx = cursorPos.x;
+        inputGlobs.msy = cursorPos.y;
+    }
 }
 
 U32 Input_AnyKeyPressed()
