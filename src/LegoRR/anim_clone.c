dad794cd0c679ac6e5b1bb95c6af74369b9153b2
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2023-07-20 20:41:17 -0500

    Implemented AnimClone LWS stuff

--- src/LegoRR/anim_clone.c
+++ src/LegoRR/anim_clone.c
@@ -1,7 +1,80 @@
 #include "anim_clone.h"
+#include "mem.h"
 
 lpAnimClone AnimClone_RegisterLws(lpLws_Info scene, LPDIRECT3DRMFRAME3 root, U32 frameCount)
 {
-    // TODO: Implement AnimClone_RegisterLws
-    return NULL;
+    AnimClone data;
+    lpAnimClone orig = Mem_Alloc(sizeof(AnimClone));
+
+    orig->clonedFrom = NULL;
+    orig->animSet = NULL;
+    orig->scene = scene;
+    orig->lws = TRUE;
+
+    orig->root = root;
+    orig->root->lpVtbl->AddRef(orig->root);
+    orig->partCount = 0;
+    orig->frameCount = frameCount;
+
+    AnimClone_WalkTree(root, 0, AnimClone_FrameCountCallback, &orig->partCount);
+    orig->partArray = Mem_Alloc(sizeof(LPDIRECT3DRMFRAME3) * orig->partCount);
+
+    data.partArray = orig->partArray;
+    data.partCount = 0;
+    AnimClone_WalkTree(root, 0, AnimClone_SetupFrameArrayCallback, &data);
+
+    return orig;
+}
+
+B32 AnimClone_WalkTree(LPDIRECT3DRMFRAME3 frame, U32 level, B32 (*Callback)(LPDIRECT3DRMFRAME3 frame, void* data), void* data)
+{
+    LPDIRECT3DRMFRAMEARRAY children;
+    LPDIRECT3DRMFRAME3 child;
+    LPDIRECT3DRMFRAME child1;
+    U32 count, loop;
+    B32 finished = FALSE;
+    HRESULT r;
+
+    if (Callback(frame, data))
+        return TRUE;
+
+    if (frame->lpVtbl->GetChildren(frame, &children) == D3DRM_OK)
+    {
+        count = children->lpVtbl->GetSize(children);
+        for (loop = 0; loop < count; loop++)
+        {
+            children->lpVtbl->GetElement(children, loop, &child1);
+            child1->lpVtbl->QueryInterface(child1, &IID_IDirect3DRMFrame3, &child);
+            child1->lpVtbl->Release(child1);
+            if (AnimClone_WalkTree(child, level + 1, Callback, data))
+            {
+                finished = TRUE;
+                r = child->lpVtbl->Release(child);
+                break;
+            }
+
+            r = child->lpVtbl->Release(child);
+        }
+
+        r = children->lpVtbl->Release(children);
+    }
+    return finished;
+}
+
+B32 AnimClone_FrameCountCallback(LPDIRECT3DRMFRAME3 frame, void* data)
+{
+    // Count the total number of frames within a given hierarchy...
+
+    U32* count = data;
+    (*count)++;
+    return FALSE; // Don't exit loop...
+}
+
+B32 AnimClone_SetupFrameArrayCallback(LPDIRECT3DRMFRAME3 frame, void* p)
+{
+    // Build the hierarchy into a table by building an array of pointers to each frame...
+
+    lpAnimClone data = p;
+    data->partArray[data->partCount++] = frame;
+    return FALSE;
 }
--- src/LegoRR/anim_clone.h
+++ src/LegoRR/anim_clone.h
@@ -21,3 +21,8 @@ typedef struct AnimClone
 }AnimClone, *lpAnimClone;
 
 extern lpAnimClone AnimClone_RegisterLws(lpLws_Info scene, LPDIRECT3DRMFRAME3 root, U32 frameCount);
+
+extern B32 AnimClone_WalkTree(LPDIRECT3DRMFRAME3 frame, U32 level, B32 (*Callback)(LPDIRECT3DRMFRAME3 frame, void* data), void* data);
+
+extern B32 AnimClone_FrameCountCallback(LPDIRECT3DRMFRAME3 frame, void* data);
+extern B32 AnimClone_SetupFrameArrayCallback(LPDIRECT3DRMFRAME3 frame, void* p);
