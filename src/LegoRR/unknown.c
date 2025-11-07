af6325ff85cdf6be82bfc700d11903a1952e8dc5
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2025-11-07 16:45:53 -0600

    Updated comment to clarify

--- src/LegoRR/unknown.c
+++ src/LegoRR/unknown.c
@@ -2,5 +2,5 @@
 
 void Unknown_Empty_FUN_00410195_LegoStripped(lpContainer cont, lpContainer camCont, U32 unkParam3)
 {
-    // Empty in final and LegoStripped
+    // Empty in final, LegoStripped, and Lego008
 }
